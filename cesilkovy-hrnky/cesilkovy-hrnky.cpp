8e61b774180c4938c335834c28d0476ce97c826c
Tomichi <michna12@gmail.com>
2015-11-20 21:11:54 +0100

    cesilkovy hrnky

---
+++ cesilkovy-hrnky/cesilkovy-hrnky.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, first, load, previous, breaks = 0;
+	bool corect = true;
+	cin >> n;
+	if (n != 0) {
+		cin >> first;
+		previous = first;
+	}
+
+	for (int i = 1; i < n; i++) {
+		cin >> load;
+		if (load < previous) {
+			corect = false;
+			++breaks;
+			if (breaks > 1) break;
+		}
+		previous = load;
+	}
+	
+	if (corect || (breaks == 1 && first >= load)) {
+		cout << "Lze preskaldat\n";
+	} else {
+		cout << "Nelze preskladat\n";
+	}
+
+	return 0;
+}
